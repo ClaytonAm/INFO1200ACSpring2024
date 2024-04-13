c3b0a27fcd813e66c6ac8e9798a97202b4b234df
ClaytonAm <claytonammon84@gmail.com>
2024-04-13 15:23:34 -0600

    Final Versions
    Finalized, added comments

--- M11/Clayton_M11.py
+++ M11/Clayton_M11.py
@@ -11,43 +11,102 @@
 
 import csv, sys
 import tkinter as tk
-from tkinter import ttk, messagebox, filedialog 
 
 FILENAME = 'contacts.csv'
 
+#I accidentally made the GUI version of this without saving the normal version,
+#so I had to redo this lame version
+
 def display_title():
-    print("Clayton's Contact Mangager App\n")
+    print("Clayton's Contact Manager App\n")    #title
 
 def display_menu():
-    
-    print("COMMAND MENU\n")
-
-    print("""list - Display all contacts\n
-            view - View a contact\n
-            add - Add a contact\n
-            del - Delete a contact\n
-            exit - Exit program\n""")
+    print("COMMAND MENU\n")     #made most of this all in one print statement for fun
+    print("""list - Display all contacts    
+view - View a contact
+add - Add a contact
+del - Delete a contact
+exit - Exit program\n""")
 
 def read_contacts():
-    global FILENAME
-    contacts = []
+    contacts = []       #create a new empty list
     try:
-        with open(FILENAME, newline="") as File:
-            reader = csv.reader(File)
-            for row in reader:
-                contacts.append(row)
-    except FileNotFoundError:
-        messagebox.showerror(message="Could not find contacts file! Starting new contacts file...")
+        with open(FILENAME, newline="") as File:    #open the csv
+            reader = csv.reader(File)       #create a csv reader object
+            for row in reader:      #iterate through each row in the csv,
+                contacts.append(row)#each row becomes a list within the contacts list
+    except FileNotFoundError:   
+        print("Could not find contacts file! Starting new contacts file...")
     
-    return contacts
+    return contacts     #return the contacts list, even if the file doesn't already exist
+
+def display(contacts):
+    if len(contacts) != 0:  #there has to be something in the contacts list
+        for i, row in enumerate(contacts, start=1):     #iterate, assign a number to each
+            print(f"{i}. {row[0]}")     #prints the numbering and the name(index[0]) of each contact
+
+    else:
+        print("There are no contacts in the list")
+        return contacts
+
+def view(contacts):
+    number = get_contact_number(contacts)   #basically get the index number
+    if number > 0:  #validation
+        contact = contacts[number-1]    #correct for off-by-one error
+        print("Name:", contact[0])  #print first index
+        print("Email:", contact[1]) #print second index
+        print("Phone:", contact[2]) #print third index
+        print() 
+
+def get_contact_number(contacts):
+    while True:
+        try:
+            number = int(input("Number: ")) #user input
+        except ValueError:
+            print("Invalid integer.\n") #validation
+            return -1   #returns this so the view() will not print anything
+            
+        if number < 1 or number > len(contacts): #validation
+            print("Invalid contact number.\n")
+            return -1   #returns this so the view() will not print anything
+        else:
+            return number
+        
+def add(contacts):
+    name = input("Name: ")  #user inputs for contact info
+    email = input("Email: ")
+    phone = input("Phone: ")
+    contact = []      #make a list for each contact
+    contact.append(name)    #add each input to the contact list
+    contact.append(email)
+    contact.append(phone)
+    contacts.append(contact)    #add the contact list to the larger contacts list
+    write_contacts(contacts)    #write the larger contacts list to csv
+    print(f"{contact[0]} was added.")   #feedback
+
+def delete(contacts):
+    number = get_contact_number(contacts)   #get user input
+    if number > 0:  #make sure it's valid
+        contact = contacts.pop(number-1)    #pop that index item from the list
+        print(f"{contact[0]} was deleted.\n")   #feedback
+    write_contacts(contacts)    #write to csv file
+
+def write_contacts(contacts):
+    with open(FILENAME, "w", newline='') as File:   #opens the file in write mode
+        writer = csv.writer(File)   #csv writer object
+        writer.writerows(contacts)  #writes each item in contacts to a row in the csv
+
+def exit_program():
+    print("Terminating program...")
+    sys.exit()  #kill it
 
 def main():
-    contacts = read_contacts()
-    display_title()
-    display_menu()
+    contacts = read_contacts() #get a list from the csv file
+    display_title() #self explanatory
+    display_menu()  #^
 
     while True:
-        command = input("Command: ")
+        command = input("Command: ")    #all self explanatory. Take user input.
         if command == "list":
             display(contacts)
         elif command == "view":
@@ -61,136 +120,6 @@ def main():
         else:
             print("Not a valid command. Please try again.\n")
     print("Bye!")
-
-def display(contacts):
-    
-    if len(contacts) != 0:
-        for i, row in enumerate(contacts, start=1):
-            print(f"{i}. {row[0]}\n")
-
-    else:
-        print("There are no contacts in the list")
-        return contacts
-
-def view(contacts):
-    number = get_contact_number
-    if number > 0:
-        contact = contacts[number-1]
-        print("Name:", contact[0])
-        print("Email:", contact[1])
-        print("Phone:", contact[2])
-        print() 
-
-def get_contact_number(contacts):
-    while True:
-        try:
-            number = int(input("Number: "))
-        except ValueError:
-            print("Invalid integer.\n")
-            return -1
-            
-        if number < 1 or number > len(contacts):
-            print("Invalid contact number.\n")
-            return -1
-        else:
-            return number
-        
-def add():
-    def save_contacts():
-        name = name_entry.get()
-        email = email_entry.get()
-        phone = phone_entry.get()
-
-        contact = []
-        contact.append(name)
-        contact.append(email)
-        contact.append(phone)
-        contacts.append(contact)
-        write_contacts()
-
-        messagebox.showinfo(message=f"{contact[0]} was added.\n")
-    
-    add_contact_window = tk.Toplevel(root)
-    add_contact_window.title("Add Contact")
-
-    tk.Label(add_contact_window, text="Name:").grid(column=0,row=0)
-    name_entry = tk.Entry(add_contact_window)
-    name_entry.grid(column=1, row=0)
-
-    tk.Label(add_contact_window, text="Email").grid(column=0,row=1)
-    email_entry = tk.Entry(add_contact_window)
-    email_entry.grid(column=1,row=1)
-
-    tk.Label(add_contact_window, text="Phone").grid(column=0,row=2)
-    phone_entry = tk.Entry(add_contact_window)
-    phone_entry.grid(column=1,row=2)
-
-    tk.Button(add_contact_window,text="Save Contact",command=save_contacts).grid(column=0,columnspan=2,row=3)
-
-def delete():
-    def delete_selected():
-        try:
-            selected = int(deletion_entry.get())
-            length = len(contacts)
-            if selected >= 1 and selected <= length:
-                contact = contacts.pop(selected-1)
-                write_contacts()
-                messagebox.showinfo(message=f"Contact: {contact} was deleted.")
-                refresh_listbox()
-            else:
-                messagebox.showerror(message="Enter a valid number")
-        except ValueError:
-            messagebox.showerror("Invalid input")
-
-    def refresh_listbox():
-        contacts_listbox.delete(0, tk.END)
-        for i, contact in enumerate(contacts, start=1):
-            text = f"{i}. {contact[0]}"
-            contacts_listbox.insert(tk.END, text)
-
-    contacts = read_contacts()
-
-    delete_contacts_window = tk.Toplevel(root)
-    delete_contacts_window.title("Delete Contacts")
-
-    contacts_frame = ttk.Frame(delete_contacts_window)
-    contacts_frame.grid(row=0, column=1, rowspan=4,sticky="ne")
-
-    contacts_listbox = tk.Listbox(contacts_frame)
-    contacts_listbox.grid(row=0,column=0,rowspan=4, sticky="ne")
-
-    contacts_scrollbar = ttk.Scrollbar(contacts_frame, orient=tk.VERTICAL,command=contacts_listbox.yview)
-    contacts_scrollbar.grid(row=0,column=1,sticky="ns")
-    contacts_listbox.config(yscrollcommand=contacts_scrollbar.set)
-    
-    for i,contact in enumerate(contacts, start=1):
-        text = f"{i}. {contact[0]}"
-        contacts_listbox.insert(tk.END, text)
-
-    tk.Label(delete_contacts_window, text="Enter contact number to delete").grid(column=0,row=0)
-    deletion_entry = tk.Entry(delete_contacts_window)
-    deletion_entry.grid(column=0,row=1)
-
-    tk.Button(delete_contacts_window,text="Delete Selected",command=delete_selected).grid(column=0,row=2)
-
-def write_contacts():
-    global FILENAME
-    with open(FILENAME, "w", newline='') as File:
-        writer = csv.writer(File)
-        writer.writerows(contacts)
-
-def import_file():
-    global FILENAME
-    FILENAME = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
-    if FILENAME:
-        messagebox.showinfo(message=f"Successfully imported file: {FILENAME}")
-    else:
-        messagebox.showinfo(message="Import failed")
-    return FILENAME
-
-def exit_program():
-    messagebox.showinfo(message="Terminating program...")
-    sys.exit()
 
 if __name__ == "__main__":  
     main()
--- M11/Clayton_M11EXTRA.py
+++ M11/Clayton_M11EXTRA.py
@@ -13,6 +13,7 @@ import csv, sys
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog 
 
+#created global variables so I don't have to constantly pass them through the functions.
 FILENAME = ''
 contacts = []
 
@@ -24,7 +25,7 @@ def read_contacts():
             reader = csv.reader(File)
             for row in reader:
                 contacts.append(row)
-    except FileNotFoundError:
+    except FileNotFoundError:       #i used messagebox for errors, so any exceptions will pop up in a separate little window
         messagebox.showerror(message="Could not find contacts file! Starting new contacts file...")
     
     return contacts
@@ -33,48 +34,34 @@ def view():
     global contacts
     contacts = read_contacts()
     if len(contacts) > 0:
-        view_contacts_window = tk.Toplevel()
+        view_contacts_window = tk.Toplevel()    #I learned about the TopLevel widget for making a separate window
         view_contacts_window.title("Your contacts")
-        listbox = tk.Listbox(view_contacts_window, width=50)
+        listbox = tk.Listbox(view_contacts_window, width=50)    #used listboxes to show outputs of variable lengths
         listbox.grid(row=0,column=0,columnspan=4)
-        for i, row in enumerate(contacts, start=1):
+        for i, row in enumerate(contacts, start=1): #this writes data from the contacts list to the listbox
             listbox.insert(tk.END, f"{i}. {row[0]} - {row[1]} - {row[2]}\n")
     else:
         messagebox.showerror(message="There are no contacts in the list")
-
-def get_contact_number(selected):
-    while True:
-        try:
-            number = selected
-        except ValueError:
-            messagebox.showerror(message="Invalid integer.\n")
-            return -1
-            
-        if number < 1 or number > len(contacts):
-            messagebox.showerror(message="Invalid contact number.\n")
-            return -1
-        else:
-            return number
         
 def add():
-    def save_contacts():
-        name = name_entry.get()
+    def save_contacts():    #functionception
+        name = name_entry.get()     #these retrieve the inputted values from the entry boxes
         email = email_entry.get()
         phone = phone_entry.get()
 
         contact = []
-        contact.append(name)
-        contact.append(email)
+        contact.append(name)    #this is basically the same as the non-gui version
+        contact.append(email)   #makes a list for each contact,
         contact.append(phone)
-        contacts.append(contact)
+        contacts.append(contact)    #adds each small contact list to the larger contactS list
         write_contacts()
 
         messagebox.showinfo(message=f"{contact[0]} was added.\n")
     
-    add_contact_window = tk.Toplevel(root)
+    add_contact_window = tk.Toplevel(root)  #opens a new window
     add_contact_window.title("Add Contact")
 
-    tk.Label(add_contact_window, text="Name:").grid(column=0,row=0)
+    tk.Label(add_contact_window, text="Name:").grid(column=0,row=0) #these are just labeled entry boxes
     name_entry = tk.Entry(add_contact_window)
     name_entry.grid(column=1, row=0)
 
@@ -86,76 +73,78 @@ def add():
     phone_entry = tk.Entry(add_contact_window)
     phone_entry.grid(column=1,row=2)
 
-    tk.Button(add_contact_window,text="Save Contact",command=save_contacts).grid(column=0,columnspan=2,row=3)
+    tk.Button(add_contact_window,text="Save Contact",command=save_contacts).grid(column=0,columnspan=2,row=3)   #it's a button.
 
 def delete():
-    def delete_selected():
+    def delete_selected():  #functionception intensifies
         try:
-            selected = int(deletion_entry.get())
-            length = len(contacts)
-            if selected >= 1 and selected <= length:
-                contact = contacts.pop(selected-1)
-                write_contacts()
-                messagebox.showinfo(message=f"Contact: {contact} was deleted.")
-                refresh_listbox()
+            selected = int(deletion_entry.get())    #user manually enters the contact to delete
+            length = len(contacts)      #salutations, validations
+            if selected >= 1 and selected <= length:    
+                contact = contacts.pop(selected-1)  #pop it
+                write_contacts()    #lock it
+                messagebox.showinfo(message=f"Contact: {contact} was deleted.") #talk it
+                refresh_listbox()   #crop it
             else:
                 messagebox.showerror(message="Enter a valid number")
         except ValueError:
             messagebox.showerror("Invalid input")
 
-    def refresh_listbox():
-        contacts_listbox.delete(0, tk.END)
-        for i, contact in enumerate(contacts, start=1):
+    def refresh_listbox():  #this function clears the listbox and displays the new contacts list,
+        contacts_listbox.delete(0, tk.END)  #after the selected entry was deleted
+        for i, contact in enumerate(contacts, start=1): #kinda proud of this idea
             text = f"{i}. {contact[0]}"
             contacts_listbox.insert(tk.END, text)
 
     contacts = read_contacts()
 
-    delete_contacts_window = tk.Toplevel(root)
+    delete_contacts_window = tk.Toplevel(root)  #new window for the contact deletion stuff
     delete_contacts_window.title("Delete Contacts")
 
-    contacts_frame = ttk.Frame(delete_contacts_window)
+    contacts_frame = ttk.Frame(delete_contacts_window)  #frame
     contacts_frame.grid(row=0, column=1, rowspan=4,sticky="ne")
 
-    contacts_listbox = tk.Listbox(contacts_frame)
+    contacts_listbox = tk.Listbox(contacts_frame)   #listbox to show the contacts
     contacts_listbox.grid(row=0,column=0,rowspan=4, sticky="ne")
 
-    contacts_scrollbar = ttk.Scrollbar(contacts_frame, orient=tk.VERTICAL,command=contacts_listbox.yview)
-    contacts_scrollbar.grid(row=0,column=1,sticky="ns")
-    contacts_listbox.config(yscrollcommand=contacts_scrollbar.set)
+    contacts_scrollbar = ttk.Scrollbar(contacts_frame, orient=tk.VERTICAL,command=contacts_listbox.yview)  
+    contacts_scrollbar.grid(row=0,column=1,sticky="ns")     #scrollbar in case the contacts list is long
+    contacts_listbox.config(yscrollcommand=contacts_scrollbar.set)  #this was surprisingly tricky to figure out
     
-    for i,contact in enumerate(contacts, start=1):
-        text = f"{i}. {contact[0]}"
-        contacts_listbox.insert(tk.END, text)
+    for i,contact in enumerate(contacts, start=1):  #iterate through the contacts list, assign a number
+        text = f"{i}. {contact[0]}"     #formatting
+        contacts_listbox.insert(tk.END, text)   #adds each iteration to the listbox
 
     tk.Label(delete_contacts_window, text="Enter contact number to delete").grid(column=0,row=0)
-    deletion_entry = tk.Entry(delete_contacts_window)
-    deletion_entry.grid(column=0,row=1)
+    deletion_entry = tk.Entry(delete_contacts_window)   #the user enters the number. I wanted to do something cooler but,
+    deletion_entry.grid(column=0,row=1)     #this works so whatever
 
-    tk.Button(delete_contacts_window,text="Delete Selected",command=delete_selected).grid(column=0,row=2)
+    tk.Button(delete_contacts_window,text="Delete Selected",command=delete_selected).grid(column=0,row=2)   #delete button
 
 def write_contacts():
-    global FILENAME
-    with open(FILENAME, "w", newline='') as File:
-        writer = csv.writer(File)
-        writer.writerows(contacts)
+    global FILENAME     #grab that global var so we can read it, but also change the value
+    with open(FILENAME, "w", newline='') as File:   #open 'er up
+        writer = csv.writer(File)   #writerbot
+        writer.writerows(contacts)  #write it
 
 def import_file():
-    global FILENAME
-    FILENAME = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
-    if FILENAME:
-        messagebox.showinfo(message=f"Successfully imported file: {FILENAME}")
+    global FILENAME     #grab the global var so we can change it
+    FILENAME = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])   #this is a cool module
+                        #this program could potentially open any CSV file. 
+    if FILENAME:        
+        messagebox.showinfo(message=f"Successfully imported file: {FILENAME}") #user feedback :)
     else:
-        messagebox.showinfo(message="Import failed")
+        messagebox.showinfo(message="Import failed")    #user feedback :(
     return FILENAME
 
 def exit_program():
     messagebox.showinfo(message="Terminating program...")
-    sys.exit()
+    sys.exit()  #kill it
 
 root = tk.Tk()
 root.title("Contact Manager App")
 
+#these are just the labels and buttons on the root window.
 ttk.Label(root, text="Clayton's Contact Manager App").grid(column=0,row=0,columnspan=2,padx=10,pady=10)
 ttk.Button(root, text="Import File", command=import_file).grid(column=0,row=1,columnspan=2)
 ttk.Button(root, text="View Contacts", command=view).grid(column=0,row=2,columnspan=2)
